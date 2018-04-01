52c32286103d63b62b4f3e547a6f616a339ff0cf
Stephan Schröder <simon.void+github@gmail.com>
2018-04-01 16:56:37 +0100

    fixed initial tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,29 @@
 let chai = require("chai");
 let chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
-let assert = chai.assert;
+chai.should();
 let { Completer, NewCompleter } = require("../dist/completer");
+
+
+
+describe('with Chai as promised it', function () {
+
+  it('should be able to resolve a promise', function () {
+    const value = "resolved with this string";
+    let promise = new Promise((resolve, reject)=>{
+      resolve(value);
+    })
+    return promise.should.become(value);
+  });
+  
+  it('should be able to reject a promise', function () {
+    const reason = "rejected with this string";
+    let promise = new Promise((resolve, reject)=>{
+      reject(reason);
+    });
+    promise.should.be.rejectedWith(reason);
+  });
+});
 
 describe('NewCompleter', function () {
   describe('#getManual()', function () {
@@ -11,14 +32,14 @@ describe('NewCompleter', function () {
       const value = "resolved with this string";
       let completer = NewCompleter.getManual();
       completer.resolve(value);
-      completer.should.become(value);
+      completer.promise.should.become(value);
     });
     
     it('should be able to reject it\'s promise', function () {
       const reason = "rejected with this string";
       let completer = NewCompleter.getManual();
       completer.reject(reason);
-      completer.should.be.rejectedWith(reason);
+      completer.promise.should.be.rejectedWith(reason);
     });
   });
 });
